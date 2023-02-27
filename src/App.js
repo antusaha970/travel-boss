0cca482e7c4486ac548669b3afba9e00aebd7951
antu <antusaha970@gmail.com>
2023-02-27 21:32:37 +0600

    Authenthecation completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
@@ -8,21 +8,30 @@ import { createContext, useState } from 'react';
 import Login from './components/Login/Login';
 
 export const PlaceContext = createContext();
+export const UserContext = createContext();
 
 function App() {
   const [currentPlace, setCurrentPlace] = useState([]);
+  const [user, setUser] = useState({
+    name: '',
+    email: ''
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 
 
   return (
     <PlaceContext.Provider value={[currentPlace, setCurrentPlace]}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/booking/:id' element={<SinglePlaceDetails />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='*' element={<NotFound />} />
+      <UserContext.Provider value={[user, setUser,isLoggedIn, setIsLoggedIn]}>
+        <Routes>
 
-      </Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/booking/:id' element={<SinglePlaceDetails />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+
+        </Routes>
+      </UserContext.Provider>
     </PlaceContext.Provider>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,123 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import NavArea from '../NavArea/NavArea';
 import './Login.css';
 import { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../Firebase/firebase';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { UserContext } from '../../App';
+
+
+
+
+const app = initializeApp(firebaseConfig);
 
 const Login = () => {
     const [isOldUser, setIsOldUser] = useState(false);
-    const [user, setUser] = useState({
-        name: '',
-        email: ''
-    });
+    const [user, setUser,isLoggedIn, setIsLoggedIn] = useContext(UserContext);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const email = document.getElementById('email').value;
+        const isEmailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+        if (!isEmailValid) {
+            alert('Please enter a valid email');
+            return;
+        }
+        const password = document.getElementById('password').value;
+        const isPasswordValid = password.length >= 6;
+        if (!isPasswordValid) {
+            alert('Please enter a valid password');
+            return;
+        }
+        if (!isOldUser) {
+            const confirmPassword = document.getElementById('confirmPassword').value;
+            const isPassMatched = password === confirmPassword;
+            if (!isPassMatched) {
+                alert('Passwords do not match');
+                return;
+            }
+        }
 
-
-
+        if (!isOldUser) {
+            const auth = getAuth();
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    alert(`Welcome ${user.email}!`);
+                    const newUser = {
+                        name: document.getElementById('name').value,
+                        email: email
+                    }
+                    setUser(newUser);
+                    setIsLoggedIn(true);
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert(errorMessage);
+                    // ..
+                });
+        }
+        else {
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    alert(`Welcome ${user.email}!`);
+                    const newUser = {
+                        name: document.getElementById('name').value,
+                        email: email
+                    }
+                    setUser(newUser);
+                    setIsLoggedIn(true);
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert(errorMessage);
+                });
+        }
     }
+
+
+    const handleGoogleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                alert(`Welcome ${user.email}!`);
+                const newUser = {
+                    name: document.getElementById('name').value,
+                    email: user.email
+                }
+                setUser(newUser);
+                setIsLoggedIn(true);
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                alert(errorMessage);
+                // ...
+            });
+    }
+
 
     return (
         <div className='bg-container-login'>
@@ -56,7 +155,7 @@ const Login = () => {
                 </div>
 
             </Container>
-            <h4 className='text-center text-white mt-2'> Continue with <span className='highlight login'>Google</span> </h4>
+            <h4 className='text-center text-white mt-2'> Continue with <span className='highlight login' onClick={handleGoogleSignIn}>Google</span> </h4>
         </div>
     );
 };
